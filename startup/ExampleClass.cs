a081ebd1f2f4e505bff9fe5893819e55667e892c
lijuyong <lijuyong@youbangsoft.com>
2018-07-19 11:32:25 +0800

    一个小小的dotnet core 用例

---
+++ startup/ExampleClass.cs
@@ -0,0 +1,11 @@
+using System;
+namespace startup
+{
+class ExampleClass
+{
+    public ExampleClass(){}
+    public ExampleClass(int v) {}
+    public void tangtang(int i) {}
+    public void lixiyan(String str) {}
+}
+}
